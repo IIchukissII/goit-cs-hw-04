3d85f5f7be4a334f002f9663e28df46cf837b6a4
Kostiantyn Romanchuk <romanchuk.konstantin@gmail.com>
2024-03-08 16:53:50 +0100

    final

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import tempfile
 import threading
 import timeit
+import logging
+import shutil
+import uuid
 from pathlib import Path
 from queue import Queue
 from tabulate import tabulate
@@ -8,7 +11,9 @@ from tabulate import tabulate
 from bm import boyer_moore_search as bm_search
 
 chunk_data = Queue()
-
+file_write_lock = threading.RLock()
+format = "%(threadName)s %(asctime)s: %(message)s"
+logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
 
 def get_files(path: Path):
     files = []
@@ -18,12 +23,13 @@ def get_files(path: Path):
     return files
 
 
-def create_temporary_files(file_path, text):
+def create_temporary_files(file_path, tmp_path, text):
     file_index = 0
     encoded_text = text.encode("utf-8")
     with tempfile.NamedTemporaryFile(
             delete=False,
-            prefix=f"chunk{file_index}_",
+            dir=Path(tmp_path),
+            prefix=str(uuid.uuid4()),
             suffix=".tmp",
     ) as tmpfile:
         tmpfile.write(encoded_text)  # Write the encoded text to the temporary file
@@ -48,12 +54,11 @@ def read_in_chunks(file_object, word_amount=1000):
         yield data
 
 
-def process_data(files):
-    tmpfile_names = []
+def process_data(files, tmp_dir):
     for file in files:
         with open(file, "r", encoding="utf-8") as f:
             for piece in read_in_chunks(f):
-                create_temporary_files(file, piece)
+                create_temporary_files(file, tmp_dir, piece)
 
 
 def find_pattern(pattern):
@@ -66,31 +71,27 @@ def find_pattern(pattern):
         with open(tmp_file, "r", encoding="utf-8-sig") as text:
             text_to_search = text.read()
             position = bm_search(text_to_search, pattern)
+            #position = re.search(pattern, text_to_search)
             if position != -1:
                 result.append((file_path, position))
 
     threads = []
     while not chunk_data.empty():
-        # Get the dictionary from the queue
         tmp_dict = chunk_data.get()
-
-        # Extract the key and value using a dictionary comprehension
         file_path, tmp_file = next(iter(tmp_dict.items()))
         thread = threading.Thread(target=search_in_file, args=(pattern, result, file_path, tmp_file))
         threads.append(thread)
         thread.start()
 
-            # Check the number of active threads
-        #active_threads = sum(1 for thread in threads if thread.is_alive())
-        #print(f"Number of active threads: {active_threads}")
+        # Check the number of active threads
+        active_threads = sum(1 for thread in threads if thread.is_alive())
+        thread_names = [thread.name for thread in threads if thread.is_alive()]
+        logging.info(f"Number of active threads: {active_threads}")
+        #logging.info(f"Number of active threads: {active_threads} with threads: {thread_names}")
 
     for thread in threads:
         thread.join()
     return result
-
-
-# Create a threading lock
-file_write_lock = threading.RLock()
 
 
 def save_results_to_txt(results, file_path, pattern=""):
@@ -103,19 +104,35 @@ def save_results_to_txt(results, file_path, pattern=""):
 
     with file_write_lock:
         with open(file_path, "w") as file:
-            file.write(f"Results for pattern '{pattern}':\n\n")
+            file.write(f"Results for pattern '{pattern}':\n")
             table_data = [(orig_file, count) for orig_file, count in result_dict.items()]
             table = tabulate(table_data, headers=["File Path", "Count"], tablefmt="grid")
             file.write(table)
 
 
 def main():
+    tmp_dir = Path(r".\tmp")
     files = get_files(Path(r".\texts"))
-    pattern = "God"
-    process_data(files)
-    save_results_to_txt(find_pattern(pattern), "results.txt", pattern)
+    pattern = "love"
+    file_name = "results.txt"
+    if not Path.exists(tmp_dir):
+        try:
+            Path.mkdir(tmp_dir)
+        except OSError:
+            print(f"Creation of the directory {tmp_dir} failed")
+
+    process_data(files, tmp_dir)
+    save_results_to_txt(find_pattern(pattern), file_name, pattern)
+
+    if Path.exists(tmp_dir):
+        try:
+            shutil.rmtree(tmp_dir)
+        except OSError:
+            print(f"Deletion of the directory {tmp_dir} failed" )
 
 
 if __name__ == "__main__":
+    time = timeit.timeit("main()", number=5)
     main()
-    timeit.timeit("main()", number=1)
+    with open("results.txt", "a") as file:
+        file.write(f"\nTime: {round(time, 3)}")
